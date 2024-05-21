56867307a143ea2cefcae0b935e62769f6b2bbfb
Smit Vekaria <smitvekaria@Smits-MBP.lan>
2024-05-20 23:31:47 -0400

    added skills section

---
+++ src/components/skills/Backend.jsx
@@ -0,0 +1,72 @@
+import React from "react";
+
+const Backend = () => {
+  return (
+    <div className="skills__content">
+      <h3 className="skills__title">Backend Developer</h3>
+
+      <div className="skills__box">
+        <div className="skills__group">
+          <div className="skills__data">
+            <i class="bx bx-badge-check"></i>
+            <div>
+              <h3 className="skills__name">PHP
+              </h3>
+                <span className="skills__level">Intermediate</span>
+            </div>
+          </div>
+
+          <div className="skills__data">
+            <i class="bx bx-badge-check"></i>
+            <div>
+              <h3 className="skills__name">Node.js
+              </h3>
+                <span className="skills__level">Basic</span>
+            </div>
+          </div>
+
+          <div className="skills__data">
+            <i class="bx bx-badge-check"></i>
+            <div>
+              <h3 className="skills__name">Python
+              </h3>
+                <span className="skills__level">Intermediate</span>
+            </div>
+          </div>
+        </div>
+
+        <div className="skills__group">
+        <div className="skills__data">
+            <i class="bx bx-badge-check"></i>
+            <div>
+              <h3 className="skills__name">AWS
+              </h3>
+                <span className="skills__level">Intermediate</span>
+            </div>
+          </div>
+
+          <div className="skills__data">
+            <i class="bx bx-badge-check"></i>
+            <div>
+              <h3 className="skills__name">MySQL
+              </h3>
+                <span className="skills__level">Intermediate</span>
+            </div>
+          </div>
+
+          <div className="skills__data">
+            <i class="bx bx-badge-check"></i>
+            <div>
+              <h3 className="skills__name">Firebase
+              </h3>
+                <span className="skills__level">Intermediate</span>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Backend;
